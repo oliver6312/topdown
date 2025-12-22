31d96a56d25e6b660b46d066621ebc62a5fc610a
oliver6312 <oliver.juhl.05@gmail.com>
2025-12-22 03:42:03 +0100

    Initial commit

---
+++ playercharacter.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+#var for movement
+@export var speed = 250
+@export var acceleration = 0.04
+@export var friction = 0.2
+#var for hands
+@onready var left_hand: Area2D = $Hands/LeftHand
+@onready var right_hand: Area2D = $Hands/RightHand
+@onready var left_hold: Marker2D = $Hands/LeftHand/Marker2D
+@onready var right_hold: Marker2D = $Hands/RightHand/Marker2D
+#lists of items currently overlapping that hand’s area.
+var left_candidates: Array[Node2D] = []
+var right_candidates: Array[Node2D] = []
+#the item currently held in each hand.
+var left_held: Node2D = null
+var right_held: Node2D = null
+
+
+func get_input():
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if (Input.get_vector("move_left", "move_right", "move_up", "move_down") != Vector2.ZERO):
+		velocity = velocity.lerp(input_direction * speed, acceleration)
+	else:
+		velocity = velocity.lerp(Vector2.ZERO, friction)
+
+func _physics_process(delta):
+	var mouse_pos := get_global_mouse_position()
+	look_at(mouse_pos)
+	get_input()
+	move_and_slide()
+
+func _on_left_hand_area_entered(area: Area2D) -> void:
+	_add_candidate(left_candidates, area)
+
+func _on_left_hand_area_exited(area: Area2D) -> void:
+	_remove_candidate(left_candidates, area)
+
+func _on_left_hand_body_entered(body: Node2D) -> void:
+	_add_candidate(left_candidates, body)
+
+func _on_left_hand_body_exited(body: Node2D) -> void:
+	_remove_candidate(left_candidates, body)
